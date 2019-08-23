8136d5eb67c485b33ceae796984350b8565bc17f
jarod429 <54276838+jarod429@users.noreply.github.com>
2019-08-23 10:29:40 -0500

    Update ejercicio4.cpp

--- ejercicio4.cpp
+++ ejercicio4.cpp
@@ -1,9 +1,11 @@
 
+#include <iostream>
+using namespace std;
 int main() {
-    i = 1;
-
+    int i = 1;
+   
     // Post-incremento asigna i a c y luego incrementa i
-    c = i++;
+     int c = i++;
     cout<<"Post-incremento ejemplo:\n";
     cout<<"El valor de c es: "<<c<<"\n";
     cout<<"El valor de i es: "<<i<<"\n";
