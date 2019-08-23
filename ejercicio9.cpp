781149e1040e2ffd2a7f6662cc8e7114d5aecad1
jarod429 <54276838+jarod429@users.noreply.github.com>
2019-08-23 10:38:30 -0500

    Update ejercicio9.cpp

--- ejercicio9.cpp
+++ ejercicio9.cpp
@@ -1,13 +1,15 @@
-#include <iostream>
+#include<iostream>
 using std::cout;
 
 int main() {
 
     int i = 1;
     while (i <= 10) {
-        if (   ) {
-            //imprimir numeros pares verticalmente
+        if ( i%2==0  ) {
+             //imprimir numeros pares verticalmente
+             cout<<i<<"\n";
+             i++;
         }
-
+        else i++; 
     }
 }
